cc254b85c1277f97f319d882cf543d73ee9172e2
tom1484 <angelus0918866416@gmail.com>
2020-02-10 21:19:21 +0800

    update application

--- Navigation/app/src/main/java/com/example/navigation/Map.java
+++ Navigation/app/src/main/java/com/example/navigation/Map.java
@@ -6,10 +6,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.TextureView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 public class Map extends TextureView {
@@ -32,9 +36,29 @@ public class Map extends TextureView {
         globalVariable.mapEvents = new ArrayList<>();
         mapEvents = globalVariable.mapEvents;
 
+        loadEvents();
+
     }
 
     private void loadEvents() {
+
+        String fileName = "data/map.json";
+
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        System.out.println("File Found : " + file.exists());
+
+        String content = null;
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                content = new String(Files.readAllBytes(file.toPath()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(content);
 
     }
 
