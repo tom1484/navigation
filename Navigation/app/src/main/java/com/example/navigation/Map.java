54f7e26db52145e1615b5136ef0267cf3e6f4fe6
tom1484 <angelus0918866416@gmail.com>
2020-02-11 02:05:05 +0800

    update application

--- Navigation/app/src/main/java/com/example/navigation/Map.java
+++ Navigation/app/src/main/java/com/example/navigation/Map.java
@@ -1,25 +1,42 @@
 package com.example.navigation;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.TextureView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Map extends TextureView {
 
-    GlobalVariable globalVariable;
-    ArrayList<MapEvent> mapEvents;
+    private GlobalVariable globalVariable;
+    private ArrayList<MapEvent> mapEvents;
+
+    private Matrix transformMatrix;
+    private Point lastPivot;
 
     public Map(Context context) {
         this(context, null);
@@ -33,41 +50,136 @@ public class Map extends TextureView {
         super(context, attrs, defStyleAttr);
 
         globalVariable = (GlobalVariable) context.getApplicationContext();
-        globalVariable.mapEvents = new ArrayList<>();
-        mapEvents = globalVariable.mapEvents;
-
         loadEvents();
+
+        transformMatrix = new Matrix();
 
     }
 
-    private void loadEvents() {
+    private MapEvent checkOnTouchEvent() {
 
-        String fileName = "data/map.json";
+        return null;
 
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    }
 
-        File file = new File(classLoader.getResource(fileName).getFile());
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
 
-        System.out.println("File Found : " + file.exists());
+//        Log.i("TAG", motionEvent.toString());
 
-        String content = null;
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                content = new String(Files.readAllBytes(file.toPath()));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        Point pivot = getPosFromMotionEvent(motionEvent);
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            lastPivot = getPosFromMotionEvent(motionEvent);
         }
-        System.out.println(content);
+
+        if (motionEvent.getPointerCount() == 1) {
+//            Log.i("TAG", String.valueOf(motionEvent == lastMotion));
+            transformMatrix.preTranslate(
+                    pivot.x - lastPivot.x,
+                    pivot.y - lastPivot.y
+            );
+        }
+
+        draw();
+        lastPivot = getPosFromMotionEvent(motionEvent);
+
+        return true;
+    }
+
+    private Point getPosFromMotionEvent(MotionEvent motionEvent) {
+
+        if (motionEvent.getPointerCount() == 1) {
+            return new Point(
+                    (int)motionEvent.getX(),
+                    (int)motionEvent.getY()
+            );
+        } else {
+            return new Point(
+                    (int)(motionEvent.getX(0) + motionEvent.getX(1)) / 2,
+                    (int)(motionEvent.getY(0) + motionEvent.getY(1)) / 2
+            );
+        }
 
     }
 
     private void draw() {
         Canvas canvas = lockCanvas(new Rect(0, 0, getWidth(), getHeight()));
 
+        float[] point = {300, 300};
+        Log.i("TAG", transformMatrix.toString());
+        transformMatrix.mapPoints(point, point);
 
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawPoint(point[0], point[1], paint);
 
         unlockCanvasAndPost(canvas);
+    }
+
+    private void loadEvents() {
+
+        globalVariable.mapEvents = new ArrayList<>();
+        mapEvents = globalVariable.mapEvents;
+
+        String jsonString = getJsonString(R.raw.map);
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray jsonEvents = jsonObject.getJSONArray("events");
+
+            for (int i = 0; i < jsonEvents.length(); i ++) {
+                JSONObject jsonEvent = (JSONObject) jsonEvents.get(i);
+                if (jsonEvent.getString("type").equals("format")) {
+                    continue;
+                }
+
+                MapEvent mapEvent = new MapEvent(
+                        jsonEvent.getInt("id"),
+                        jsonEvent.getString("type"),
+                        jsonEvent.getString("name"),
+                        jsonEvent.getString("description"),
+                        jsonEvent.getJSONArray("items"),
+                        jsonEvent.getDouble("x"),
+                        jsonEvent.getDouble("y"),
+                        jsonEvent.getDouble("width"),
+                        jsonEvent.getDouble("height")
+                );
+
+                mapEvents.add(mapEvent);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+//        Log.i("TAG", globalVariable.mapEvents.toString());
+    }
+
+    public String getJsonString(int id) {
+
+        InputStream is = getResources().openRawResource(id);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return writer.toString();
     }
 
 }
--- Navigation/app/src/main/java/com/example/navigation/MapEvent.java
+++ Navigation/app/src/main/java/com/example/navigation/MapEvent.java
@@ -1,7 +1,61 @@
 package com.example.navigation;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MapEvent {
 
+    public int id;
+    public String type;
+    public String name;
+    public String description;
+    public ArrayList<Integer> items;
+    public double x;
+    public double y;
+    public double width;
+    public double height;
+
     public MapEvent() {}
+
+    public MapEvent(int _id, String _type, String _name, String _description, JSONArray _items, double _x, double _y, double _width, double _height) {
+
+        id = _id;
+        type = _type;
+        name = _name;
+        description = _description;
+        items = new ArrayList<>();
+        for (int i = 0; i < _items.length(); i ++) {
+            try {
+                items.add(_items.getInt(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        x = _x;
+        y = _y;
+        width = _width;
+        height = _height;
+
+    }
+
+    public String toString() {
+
+        String string = "{";
+        string += "id: " + String.valueOf(id) + ", ";
+        string += "type: " + String.valueOf(type) + ", ";
+        string += "name: " + String.valueOf(name) + ", ";
+        string += "description: " + String.valueOf(description) + ", ";
+        string += "items: " + String.valueOf(items) + ", ";
+        string += "x: " + String.valueOf(x) + ", ";
+        string += "y: " + String.valueOf(y) + ", ";
+        string += "width: " + String.valueOf(width) + ", ";
+        string += "height: " + String.valueOf(height) + "}";
+
+        return string;
+
+    }
 
 }
